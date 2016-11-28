2f03d566756cd7a8f5825aa9a1019a944125ecee
pgomez <pgomez@visual-engin.com>
2016-11-28 12:36:56 +0100

    Se genera documentación

---
+++ SwiftProtocolsAndGeneric/SwiftProtocolsAndGeneric/SwiftProtocolsAndGeneric.h
@@ -0,0 +1,19 @@
+//
+//  SwiftProtocolsAndGeneric.h
+//  SwiftProtocolsAndGeneric
+//
+//  Created by pgomez on 28/11/16.
+//  Copyright © 2016 pgomez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftProtocolsAndGeneric.
+FOUNDATION_EXPORT double SwiftProtocolsAndGenericVersionNumber;
+
+//! Project version string for SwiftProtocolsAndGeneric.
+FOUNDATION_EXPORT const unsigned char SwiftProtocolsAndGenericVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftProtocolsAndGeneric/PublicHeader.h>
+
+
